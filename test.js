0e59c64d132c8a38819c4835eb25b64d4c73ac8f
Insikynwa <pavel.churkin@mail.ru>
2021-03-30 23:16:21 +0300

    new example

---
+++ test.js
@@ -0,0 +1,39 @@
+/*const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+const towelSort = (array) => {
+  const expectedArray = [];
+
+  array.forEach((item, index) => {
+    if (index % 2 !== 0) {
+      expectedArray.push(...item.reverse());
+    } else {
+      expectedArray.push(...item);
+    }
+  });
+
+  return expectedArray;
+};
+console.log(towelSort(matrix));
+
+*/
+const str = true;
+const options = { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' };
+
+class VigenereCipheringMachine {
+  encrypt() {
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+  }    
+  decrypt() {
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+  }
+}
+
+
+console.log(encrypt());
